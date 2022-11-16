af33442e88b605bdaae11156f998c82316a725e5
Michael Jecmen <pmjecmen@twitch.tv>
2022-11-16 15:30:15 -0800

    add player test script

--- player.sh
+++ player.sh
@@ -7,4 +7,10 @@ echo "\t$$: running tests with live url: $LIVE_URL"
 echo "\t$$: running tests with vod url: $VOD_URL"
 echo "\t$$: running tests with clip url: $CLIP_URL"
 
-echo "\t$$: all tests pass!"
+# fail if PID is odd, pass if even
+if [ $(($$%2)) -eq 0 ]; then
+    echo "\t$$: test FAILED"
+    exit(1)
+fi
+echo "\t$$: test PASSED"
+exit(0)
