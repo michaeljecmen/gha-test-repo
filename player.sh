29f933825227c9e7afaf35f7d71ea2b9060b8eb1
Michael Jecmen <pmjecmen@twitch.tv>
2022-11-16 15:34:06 -0800

    player script cleanup

--- player.sh
+++ player.sh
@@ -3,14 +3,14 @@
 # pass name of test script as command line arg
 # and the input urls as environment variables
 echo "$$: running $1 tests"
-echo "\t$$: running tests with live url: $LIVE_URL"
-echo "\t$$: running tests with vod url: $VOD_URL"
-echo "\t$$: running tests with clip url: $CLIP_URL"
+echo "    $$: running tests with live url: $LIVE_URL"
+echo "    $$: running tests with vod url: $VOD_URL"
+echo "    $$: running tests with clip url: $CLIP_URL"
 
 # fail if PID is odd, pass if even
 if [ $(($$%2)) -eq 0 ]; then
-    echo "\t$$: test FAILED"
-    exit(1)
+    echo "    $$: test FAILED"
+    exit 1
 fi
-echo "\t$$: test PASSED"
-exit(0)
+echo "    $$: test PASSED"
+exit 0
