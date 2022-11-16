579ff209f5a7d6612bf921478ff7317490cafe5c
Michael Jecmen <pmjecmen@twitch.tv>
2022-11-15 16:43:54 -0800

    script

---
+++ generate-fuzzy-inputs.py
@@ -0,0 +1,8 @@
+import json
+
+bundle = {
+	"live": "https://www.twitch.tv/bufferbunny",
+	"vod": "https://www.twitch.tv/videos/1652600015",
+	"clip": "https://www.twitch.tv/hasanabi/clip/BlightedCrackyCaterpillarBabyRage-FPljWm6tgOFhAJAV"
+}
+print(json.dumps(bundle))
