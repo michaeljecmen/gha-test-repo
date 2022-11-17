f6ae2f157ce911662eb703010a18d5b761367a1c
Michael Jecmen <pmjecmen@twitch.tv>
2022-11-16 16:51:35 -0800

    use print with set-output instead of os.environ append

--- generate-fuzzy-inputs.py
+++ generate-fuzzy-inputs.py
@@ -4,6 +4,12 @@ live="https://www.twitch.tv/bufferbunny",
 vod="https://www.twitch.tv/videos/1652600015",
 clip="https://www.twitch.tv/hasanabi/clip/BlightedCrackyCaterpillarBabyRage-FPljWm6tgOFhAJAV"
 
-os.environ['GITHUB_OUTPUT'] += f'live-web={live}'
-os.environ['GITHUB_OUTPUT'] += f'vod-web={vod}'
-os.environ['GITHUB_OUTPUT'] += f'clip-web={clip}'
+# does not work
+# os.environ['GITHUB_OUTPUT'] += f'live-web={live}'
+# os.environ['GITHUB_OUTPUT'] += f'vod-web={vod}'
+# os.environ['GITHUB_OUTPUT'] += f'clip-web={clip}'
+
+# https://github.com/orgs/community/discussions/28146
+print(f'::set-output live-web={live}')
+print(f'::set-output vod-web={vod}')
+print(f'::set-output clip-web={clip}')
