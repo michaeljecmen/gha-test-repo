517d6194fd11bd099f813bb8a007cdaa2a2e18ab
Michael Jecmen <pmjecmen@twitch.tv>
2022-11-16 16:45:31 -0800

    shift to using separate inputs and outputs for each kind of url

--- generate-fuzzy-inputs.py
+++ generate-fuzzy-inputs.py
@@ -1,8 +1,9 @@
-import json
+import os
 
-bundle = {
-	"live": "https://www.twitch.tv/bufferbunny",
-	"vod": "https://www.twitch.tv/videos/1652600015",
-	"clip": "https://www.twitch.tv/hasanabi/clip/BlightedCrackyCaterpillarBabyRage-FPljWm6tgOFhAJAV"
-}
-print(json.dumps(bundle))
+live="https://www.twitch.tv/bufferbunny",
+vod="https://www.twitch.tv/videos/1652600015",
+clip="https://www.twitch.tv/hasanabi/clip/BlightedCrackyCaterpillarBabyRage-FPljWm6tgOFhAJAV"
+
+os.environ['GITHUB_OUTPUT'] += f'live-web={live}'
+os.environ['GITHUB_OUTPUT'] += f'vod-web={vod}'
+os.environ['GITHUB_OUTPUT'] += f'clip-web={clip}'
--- player.sh
+++ player.sh
@@ -3,13 +3,9 @@
 # pass name of test script as command line arg
 # and the input urls as environment variables
 echo "$$: running $1 tests"
-
-LIVE_URL=$(python3 -c "import sys, json; print(json.load(sys.stdin)['live'])")
-VOD_URL=$(python3 -c "import sys, json; print(json.load(sys.stdin)['vod'])")
-CLIP_URL=$(python3 -c "import sys, json; print(json.load(sys.stdin)['clip'])")
-echo "    $$: running tests with live url: $LIVE_URL"
-echo "    $$: running tests with vod url: $VOD_URL"
-echo "    $$: running tests with clip url: $CLIP_URL"
+echo "    $$: running tests with live url: $2"
+echo "    $$: running tests with vod url: $3"
+echo "    $$: running tests with clip url: $4"
 
 # fail if PID is even, pass if odd
 if [ $(($$%2)) -eq 0 ]; then
