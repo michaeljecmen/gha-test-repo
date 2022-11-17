c491f587b3a553042b2e32e37dba0b150c284261
Michael Jecmen <pmjecmen@twitch.tv>
2022-11-16 16:11:06 -0800

    add json parsing to player script

--- player.sh
+++ player.sh
@@ -3,6 +3,11 @@
 # pass name of test script as command line arg
 # and the input urls as environment variables
 echo "$$: running $1 tests"
+
+URL_BUNDLE=$2
+LIVE_URL=$(python3 -c "import sys, json; print(json.load(sys.stdin)['live'])")
+VOD_URL=$(python3 -c "import sys, json; print(json.load(sys.stdin)['vod'])")
+CLIP_URL=$(python3 -c "import sys, json; print(json.load(sys.stdin)['clip'])")
 echo "    $$: running tests with live url: $LIVE_URL"
 echo "    $$: running tests with vod url: $VOD_URL"
 echo "    $$: running tests with clip url: $CLIP_URL"
